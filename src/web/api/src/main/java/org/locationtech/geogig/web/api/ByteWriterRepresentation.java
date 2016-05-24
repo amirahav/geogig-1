68e56ee7a6c00d068c39aec08341a92e400be800
jmiller@boundlessgeo.com <jmiller@boundlessgeo.com>
2016-05-24 16:40:40 -0400

    Added shapefile export to web api Log

---
+++ src/web/api/src/main/java/org/locationtech/geogig/web/api/ByteWriterRepresentation.java
@@ -0,0 +1,25 @@
+package org.locationtech.geogig.web.api;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.locationtech.geogig.rest.ByteRepresentation;
+import org.restlet.data.MediaType;
+
+public class ByteWriterRepresentation extends ByteRepresentation {
+    final ByteResponse impl;
+
+    public ByteWriterRepresentation(MediaType mediaType, ByteResponse impl) {
+        super(mediaType);
+        this.impl = impl;
+    }
+
+    @Override
+    public void write(OutputStream writer) throws IOException {
+        try {
+            impl.write(writer);
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
+    }
+}
